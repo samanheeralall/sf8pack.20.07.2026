0664b3eee91ab8a9b591b999491bdaf1e5837992
Heeralall <heeralall@MacBook-Air-de-Heeralall.local>
2026-07-23 14:13:44 +0200

    [sf-start] Exercice13 : Protect the Admin Controller

--- src/Controller/Admin/BookController.php
+++ src/Controller/Admin/BookController.php
@@ -4,14 +4,24 @@ namespace App\Controller\Admin;
 
 use App\Entity\Book;
 use App\Form\BookType;
+use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class BookController extends AbstractController
 {
+    #[Route('', name: 'app_admin_books')]
+    public function index(BookRepository $bookRepository): Response
+    {
+        return $this->render("admin/book/index.html.twig", [
+            'books' => $bookRepository->findAll(),
+        ]);
+    }
     #[Route('/admin/books/new', name: 'app_admin_book_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $em): Response
     {
